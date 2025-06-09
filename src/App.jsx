0c456f4b35109dbae7f4a00dfab31b21cb34085f
Kading82 <Kading82@gmail.com>
2025-06-08 17:57:51 -0700

    general shape of page, more custom css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import headshot from "./images/headshot_webpage.jpg"
+
+function Image() {
+  return <img width={200} height={175} src={headshot} padding={10} align="left" alt="Headshot" />;
+}
 
 function MainInput() {
   return(
@@ -23,13 +28,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <MainHeader/>
-        <h2>Web Developer with 9 years of experience.</h2>
+        <div class="mainheader">
+          <div class="item">
+            <Image/>
+            <br />
+            <MainHeader/>
+            <h2>Web Developer with 9 years of experience.</h2>
+        </div>
+          <div class="item">
+            <h2>MySQL Databases from top to botttom.</h2>
+            <p>From writing scripts to perfrom complex database operations to optimizing existing queries using such methods as indexing.</p>
+          </div>
+        </div>
+        <br />
         <MainInput/>
         <br />
         <MainButton/>
-
       </header>
     </div>
   );
